f5a00df2856ee1e8967b64cad32e24fd2f14b2b2
MateuszPatrzalek <mateuszpatrzalek@gmail.com>
2022-04-03 17:49:15 +0200

    added SplashScreen with style and assets

---
+++ lib/MyHomePage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(),
+      ),
+    );
+  }
+}
---
+++ lib/PermissionScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PermisionScreen extends StatefulWidget {
+
+  @override
+  State<PermisionScreen> createState() => _PermisionScreenState();
+}
+
+class _PermisionScreenState extends State<PermisionScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(),
+      ),
+    );
+  }
+}
